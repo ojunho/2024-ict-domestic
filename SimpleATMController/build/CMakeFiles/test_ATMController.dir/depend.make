84cbd53a499dd1777bb20cbe48811ae4db8dd194
ojunho <ojunho7535@gmail.com>
2024-07-17 20:25:16 +0900

    Remove .vscode and build directory from repository

--- SimpleATMController/build/CMakeFiles/test_ATMController.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for test_ATMController.
-# This may be replaced when dependencies are built.
--- SimpleATMController/build/CMakeFiles/test_ATMController.dir/src/Account.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/test_ATMController.dir/src/Account.cpp.o: \
-  /Users/ojunho/github/2024-ict-domestic/SimpleATMController/src/Account.cpp \
-  /Users/ojunho/github/2024-ict-domestic/SimpleATMController/include/Account.h
